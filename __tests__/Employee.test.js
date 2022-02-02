77febe8f3af942ced60790c097cfa8ba9933d041
zanescheel-fTEJAV <zane.scheel@gmail.com>
2022-02-01 19:32:43 -0600

    finished Engineer class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,25 +9,25 @@ test('create a new employee object', () => {
 
 });
 
-test('return the employee name', () => {
+test('getName return the employee name', () => {
     const employee = new Employee('John', 1, 'john@gmail.com');
 
     expect(employee.getName()).toBe('John');
 })
 
-test('returns the employee ID', () => {
+test('getId returns the employee ID', () => {
     const employee = new Employee('John', 1, 'john@gmail.com');
 
     expect(employee.getId()).toBe(1);
 });
 
-test('return the employee email', () => {
+test('getEmail return the employee email', () => {
     const employee = new Employee('John', 1, 'john@gmail.com');
 
     expect(employee.getEmail()).toBe('john@gmail.com');
 });
 
-test('returns employee role', () => {
+test('getRole returns employee role', () => {
     const employee = new Employee('John', 1, 'john@gmail.com');
 
     expect(employee.getRole()).toBe('Employee');
