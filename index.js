a670221ce912d6850546dfee758fdedef03b00a9
zanescheel-fTEJAV <zane.scheel@gmail.com>
2022-02-05 00:17:32 -0600

    finished creating questions

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const employees = [];
 
 const managerQuestions = [
     {
@@ -12,7 +14,7 @@ const managerQuestions = [
     {
         type: 'input',
         name: 'id',
-        message: 'What is their employee id?'
+        message: 'What is their employee ID?'
     },
     {
         type: 'input',
@@ -21,7 +23,7 @@ const managerQuestions = [
     },
     {
         type: 'input',
-        name: 'email',
+        name: 'office',
         message : 'What is their office number?'
     },
     {
@@ -31,10 +33,105 @@ const managerQuestions = [
     }
 ]
 
+const roleQuestion = [
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What is their postition?',
+        choices: ['Engineer', 'Intern']
+    },
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is their name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is there ID?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is their email?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is their github?'
+    },
+    {
+        type: 'confirm',
+        name: 'newEmployee',
+        message: 'Do you want to add another employee?'
+    }
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is their name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is there ID?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is their email?'
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What is their school?'
+    },
+    {
+        type: 'confirm',
+        name: 'newEmployee',
+        message: 'Do you want to add another employee?'
+    }
+]
+
+
+function nextEmployee() {
+    inquirer.prompt(roleQuestion).then(data => {
+        if(data.role === 'Engineer') {
+            inquirer.prompt(engineerQuestions).then(data => {
+                console.log(data);
+                const engineer = new Engineer(data.name, data.id, data.email, data.github);
+                employees.push(engineer);
+                if (data.newEmployee) {
+                    nextEmployee(data);
+                }
+            });
+        } else {
+            inquirer.prompt(internQuestions).then(data => {
+                console.log(data);
+                const intern = new Intern(data.name, data.id, data.email, data.school)
+                employees.push(intern);
+                if (data.newEmployee) {
+                    nextEmployee(data);
+                }
+            });
+        }
+    });
+}
+
 function manager() {
     inquirer.prompt(managerQuestions).then(data => {
         console.log(data);
-    })
+        const manager = new Manager(data.name, data.id, data.email, data.office);
+        employees.push(manager);
+        if(data.newEmployee) {
+            nextEmployee();
+        }
+    });
 }
 
 manager();
